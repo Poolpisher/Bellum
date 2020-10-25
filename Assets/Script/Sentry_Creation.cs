dc1347e31758b61c7e10aa4682b15498ef49082a
Rémy <remy.melinon.71@gmail.com>
2020-10-25 15:41:07 +0100

    Menus et Map
    Modifications des menus et de la map

---
+++ Assets/Script/Sentry_Creation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sentry_Creation : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Sentry;
+    //Rigidbody
+    private new Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //StartCoroutine(Create());
+    }
+    /*
+    private IEnumerator Create()
+    {
+        var create = Instantiate(Sentry, rigidbody.position, Quaternion.identity);
+    }
+    */
+}
