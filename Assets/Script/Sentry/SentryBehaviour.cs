68df08c9e6c90eea49778a1ef1dee58bc4c2ffcd
Rémy <remy.melinon.71@gmail.com>
2021-02-13 16:47:22 +0100

    Cours du 13/02/2021
    Réglages des différents problèmes liés à unity, les packages ont été réinstallés.
    L'ordre de lancements des scripts GameManager et Control à été modifié pour que le Awake de GameManager se lance avant le OnEnable de Control.
    La range de la tourelle s'affiche maintenant correctement lors de sa création ou sa sélection.

--- Assets/Script/Sentry/SentryBehaviour.cs
+++ Assets/Script/Sentry/SentryBehaviour.cs
@@ -16,8 +16,6 @@ public class SentryBehaviour : MonoBehaviour
     [SerializeField] private float shootTimer;
     //Temps correspondant au dernier tir
     private float lastShoot;
-    //Distance de tir maximale de la tourelle
-    [SerializeField] private float rangeShoot;
     //Distance entre le firstAgent et les canons de la tourelle dans la fonction tir
     private Vector3 look;
     //Layer des ennemies pour pouvoir trier ce qui est dans la range des tourelles
@@ -37,6 +35,7 @@ public class SentryBehaviour : MonoBehaviour
         NavMeshAgent firstAgent = null;
         //Récupère les ennemis qui entre en collision avec la range de la tourelle (2e enfant de la tourelle)
         Collider[] hitColliders = Physics.OverlapSphere(transform.position, (transform.localScale.x * transform.GetChild(1).localScale.x)/2, ennemyLayer);
+        Debug.Log(hitColliders.Length);
         //Pour chaque ennemi récupéré
         foreach (var hitCollider in hitColliders)
         {
@@ -54,6 +53,7 @@ public class SentryBehaviour : MonoBehaviour
                 firstAgent = agent;
             }
         }
+        Debug.Log(firstAgent);
         //Tire sur le FirstAgent
         Shoot(firstAgent);
     }
@@ -66,27 +66,23 @@ public class SentryBehaviour : MonoBehaviour
         {
             return;
         }
-        //Si la tourelle à la portée suffisante (rangeShoot) pour tirer sur le firstAgent
-        if (rangeShoot < Vector3.Distance(canonTransform.position, firstAgent.transform.position))
+        //Calcul du temps lors du tir
+        var actualTime = Time.time;
+
+        //Faire en sorte que les tourelles regardent l'ennemie le plus avancé de la carte (le firstAgent de EnnemyBehaviour)
+        transform.LookAt(firstAgent.transform.position);
+
+        //Permet d'instaurer le cooldown entre 2 tir
+        if (actualTime > lastShoot + shootTimer)
         {
-            //Calcul du temps lors du tir
-            var actualTime = Time.time;
-
-            //Faire en sorte que les tourelles regardent l'ennemie le plus avancé de la carte (le firstAgent de EnnemyBehaviour)
-            transform.LookAt(firstAgent.transform.position);
-
-            //Permet d'instaurer le cooldown entre 2 tir
-            if (actualTime > lastShoot + shootTimer)
-            {
-                //Création de la balle
-                var createBullet = Instantiate(bulletPrefab, canonTransform.position, Quaternion.identity);
-                //Distance entre le firstAgent et les canons de la tourelle
-                var look = (firstAgent.transform.position - canonTransform.position).normalized;
-                //Orientation de la balle
-                createBullet.GetComponent<Bullet>().fixinputValue = look;
-                //Changement de la valeur du dernier tir
-                lastShoot = actualTime;
-            }
+            //Création de la balle
+            var createBullet = Instantiate(bulletPrefab, canonTransform.position, Quaternion.identity);
+            //Distance entre le firstAgent et les canons de la tourelle
+            var look = (firstAgent.transform.position - canonTransform.position).normalized;
+            //Orientation de la balle
+            createBullet.GetComponent<Bullet>().fixinputValue = look;
+            //Changement de la valeur du dernier tir
+            lastShoot = actualTime;
         }
     }
 }
